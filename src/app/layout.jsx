e1999465e3440cfe800bc863a12077461124e6f7
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2023-10-12 20:28:28 -0300

    03-add-list-pokemons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="overflow-x-hidden h-screen">
         <Banner />
         {children}
       </body>
---
+++ src/app/pokemons/ListPokemons.jsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+
+const getPokemons = () => {
+  return fetch('https://pokeapi.co/api/v2/pokemon').then((res) => res.json())
+}
+
+const getPokemon = (url) => {
+  return fetch(`${url}`).then((res) => res.json())
+}
+
+const Page = async () => {
+  const pokemonList = await getPokemons()
+  const { results } = pokemonList || {}
+  return (
+    <div suppressHydrationWarning className="flex justify-center">
+      <div className="grid grid-cols-4 gap-x-20">
+        {results.map(async ({ name, url }) => {
+          const pokemon = await getPokemon(url)
+          const { id, sprites, types } = pokemon || {}
+          const { front_shiny } = sprites || {}
+          return (
+            <div key={id} className="mb-10">
+              <div className=" bg-slate-100 rounded-full p-8 shadow-2xl">
+                <h3 className="text-center mt-2">{name}</h3>
+                <Image
+                  width={130}
+                  height={130}
+                  alt="not found"
+                  src={`${front_shiny}`}
+                ></Image>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Page
